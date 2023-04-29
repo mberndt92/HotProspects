3a395cf8457dd616cdadf8d4da545045a6dae0cd
Mberndt92 <maximilian_berndt@icloud.com>
2023-04-30 08:29:42 +0900

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Maximilian Berndt on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
