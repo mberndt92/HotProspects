107f0aecfd8df4ea6be3e55b06a24e47e966c19f
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-01 08:43:10 +0900

    Add FileManager extension

---
+++ HotProspects/FileManager+DocumentsDirectory.swift
@@ -0,0 +1,45 @@
+//
+//  Filemanager+DocumentsDirectory.swift
+//  HotProspects
+//
+//  Created by Maximilian Berndt on 2023/04/12.
+//
+
+import Foundation
+
+extension FileManager {
+    
+    static var documentsDirectory: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    func saveInDocuments<T: Encodable>(to filename: String, data: T) {
+        let url = FileManager.documentsDirectory.appendingPathComponent(filename)
+        do {
+            let encodedData = try JSONEncoder().encode(data)
+            try encodedData.write(to: url, options: .atomic)
+        } catch {
+            fatalError("Failed to save to documents directory")
+        }
+    }
+    
+    func loadFromDocuments<T: Decodable>(_ fileName: String, fallback: T) -> T {
+        let url = FileManager.documentsDirectory.appendingPathComponent(fileName)
+        
+        do {
+            guard fileExists(atPath: url.path) else {
+                fatalError("file not found")
+            }
+            
+            guard let data = try? Data(contentsOf: url) else {
+                try FileManager.default.removeItem(atPath: url.path())
+                return fallback
+            }
+            let decodedData = try JSONDecoder().decode(T.self, from: data)
+            
+            return decodedData
+        } catch {
+            fatalError("Failed to load data from documents directory")
+        }
+    }
+}
